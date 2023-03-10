318605e47727e27dc561d28c2ade4868d4221b5c
WYZ00 <thanthtettin@gmail.com>
2023-03-10 18:10:14 +0630

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import {firebase} from "firebase/app"
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAN9ZX7bCd2szUDRjlYdWLxdDLogFYhj8s",
+    authDomain: "live-chat-e6c65.firebaseapp.com",
+    projectId: "live-chat-e6c65",
+    storageBucket: "live-chat-e6c65.appspot.com",
+    messagingSenderId: "327783714344",
+    appId: "1:327783714344:web:7ae4d4a7630d13cf686f1d"
+};
+
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+let timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export {db,timestamp}
