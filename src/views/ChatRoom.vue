f3a2f17e538daab450f95380087a362c524b392e
WYZ00 <thanthtettin@gmail.com>
2023-03-15 16:22:41 +0630

    create and design chatwindow

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <NavBar></NavBar>
+    <ChatWindow></ChatWindow>
     <NewChatForm></NewChatForm>
   </div>
 </template>
 
 <script>
+import ChatWindow from '../components/ChatWindow'
 import NewChatForm from '../components/NewChatForm'
 import { watch } from 'vue'
 import NavBar from '../components/NavBar'
@@ -13,6 +15,7 @@ import getUser from '@/composables/getUser'
 import {useRouter} from "vue-router"
 export default {
   components: {
+    ChatWindow,
     NewChatForm, NavBar },
   setup(){
     let {user} =getUser();
