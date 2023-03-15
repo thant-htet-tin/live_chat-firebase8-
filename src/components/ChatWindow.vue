144e661e6f29ef060a91a43109b06b514aece6e1
WYZ00 <thanthtettin@gmail.com>
2023-03-15 16:36:14 +0630

    Real-time firestore listener

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="chat-window">
-    <div class="messages">
+    <div class="messages" v-for="message in messages" :key="message.id">
         <div class="single">
-            <span class="created-at">3mins ago</span>
-            <span class="name">tht</span>
-            <span class="message">hi there</span>
+            <span class="created-at">{{message.created_at}}</span>
+            <span class="name">{{message.name}}</span>
+            <span class="message">{{message.message}}</span>
         </div>
-        <div class="single">
-            <span class="created-at">3mins ago</span>
-            <span class="name">tht</span>
-            <span class="message">hi there</span>
-        </div>
-        <div class="single">
-            <span class="created-at">3mins ago</span>
-            <span class="name">tht</span>
-            <span class="message">hi there</span>
-        </div>
-        <div class="single">
-            <span class="created-at">3mins ago</span>
-            <span class="name">tht</span>
-            <span class="message">hi there</span>
-        </div>
-                
+                       
     </div>
   </div>
 </template>
 
 <script>
+import { db } from '@/firebase/config'
+import { ref } from 'vue';
 export default {
+    setup(){
+        let messages = ref([]);
+        db.collection('messages').orderBy('created_at').onSnapshot(snap=>{
+            // console.log(snap.docs);
+            let results = [];
 
+            snap.docs.forEach(doc=>{
+                let document = {...doc.data(),id:doc.id}
+                // console.log(doc.data());
+                // console.log(document);
+                results.push(document);
+            });
+            messages.value = results;
+            console.log(results);
+        });
+
+        return {messages};
+    }
 }
 </script>
 
