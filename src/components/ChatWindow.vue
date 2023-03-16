78a10d1136242e81a9b44d1f929ea789e465a078
WYZ00 <thanthtettin@gmail.com>
2023-03-16 19:08:32 +0630

    changed date with fns lib and adding autoscrolling

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="chat-window">
-    <div class="messages" v-for="message in messages" :key="message.id">
-        <div class="single">
-            <span class="created-at">{{message.created_at.toDate()}}</span>
+    <div class="messages" ref="msgBox">
+        <div class="single" v-for="message in formattedMessages" :key="message.id">
+            <span class="created-at">{{message.created_at}}</span>
             <span class="name">{{message.name}}</span>
             <span class="message">{{message.message}}</span>
         </div>
@@ -13,10 +13,27 @@
 
 <script>
 import { db } from '@/firebase/config'
-import { ref } from 'vue';
+import { computed, onUpdated, ref } from 'vue';
+import {formatDistanceToNow} from 'date-fns'
+
 export default {
     setup(){
         let messages = ref([]);
+        let msgBox = ref(null);
+
+        // autoscroll
+        onUpdated(()=>{
+            // console.log(msgBox.value);
+            msgBox.value.scrollTop = msgBox.value.scrollHeight;
+        });
+
+        let formattedMessages = computed(()=>{
+            return messages.value.map(msg=>{
+                let formatTime = formatDistanceToNow(msg.created_at.toDate());
+                return {...msg,created_at:formatTime}
+            });
+        });
+
         db.collection('messages').orderBy('created_at').onSnapshot(snap=>{
             // console.log(snap.docs);
             let results = [];
@@ -34,7 +51,7 @@ export default {
             // console.log(results);
         });
 
-        return {messages};
+        return {messages,formattedMessages,msgBox};
     }
 }
 </script>
